e5c11bf9aab18d81832f40998811ad21f400ea0b
aheitzen <alexaheitzenrader@yahoo.com>
2016-04-07 10:15:44 -0700

    deploying

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -16,61 +16,22 @@ app.controller('HomeCtrl', function($scope) {
 });
 
 app.controller('d3Ctrl', function($scope) {
-    // var gauge1 = loadLiquidFillGauge("fillgauge1", 55);
-    var config1 = liquidFillGaugeDefaultSettings();
-    // config1.circleColor = "#FF7777";
-    // config1.textColor = "#FF4444";
-    // config1.waveTextColor = "#FFAAAA";
-    // config1.waveColor = "#FFDDDD";
-    // config1.circleThickness = 0.2;
-    // config1.textVertPosition = 0.2;
-    // config1.waveAnimateTime = 1000;
-    var gauge2= loadLiquidFillGauge("fillgauge2", 12.13, config1);
+    
+
     var config2 = liquidFillGaugeDefaultSettings();
     config2.circleColor = "#FFFFFF";
     config2.textColor = "#000000";
     config2.waveTextColor = "#000000";
-    config2.waveColor = "#000000";
-    config2.circleThickness = 0.1;
-    config2.circleFillGap = 0.2;
-    config2.textVertPosition = 0.8;
-    config2.waveAnimateTime = 2000;
-    config2.waveHeight = 0.3;
-    config2.waveCount = 1;
-    // var gauge3 = loadLiquidFillGauge("fillgauge3", 60.1, config2);
-    var config3 = liquidFillGaugeDefaultSettings();
-    // config3.textVertPosition = 0.8;
-    // config3.waveAnimateTime = 5000;
-    // config3.waveHeight = 0.15;
-    // config3.waveAnimate = false;
-    // config3.waveOffset = 0.25;
-    // config3.valueCountUp = false;
-    // config3.displayPercent = false;
-    var gauge4 = loadLiquidFillGauge("fillgauge4", 33.32, config3);
-    var config4 = liquidFillGaugeDefaultSettings();
-    config2.circleColor = "#FFFFFF";
-    config2.textColor = "#000000";
-    config2.waveTextColor = "#000000";
-    config2.waveColor = "#000000";
-    config2.circleThickness = 0.1;
-    config2.circleFillGap = 0.2;
-    config2.textVertPosition = 0.8;
-    config2.waveAnimateTime = 2000;
-    config2.waveHeight = 0.3;
-    config2.waveCount = 1;
-    var gauge5 = loadLiquidFillGauge("fillgauge5", 66, config4);
-    // var config5 = liquidFillGaugeDefaultSettings();
-    config2.circleColor = "#FFFFFF";
-    config2.textColor = "#000000";
-    config2.waveTextColor = "#000000";
-    config2.waveColor = "#000000";
-    config2.circleThickness = 0.1;
-    config2.circleFillGap = 0.2;
-    config2.textVertPosition = 0.8;
-    config2.waveAnimateTime = 2000;
-    config2.waveHeight = 0.3;
-    config2.waveCount = 1;
-    // var gauge6 = loadLiquidFillGauge("fillgauge6", 120, config5);
+    config2.waveColor = "#FFFFFF";
+    config2.circleThickness = 0.05;
+    config2.circleFillGap = 0.05;
+
+    loadLiquidFillGauge("fillgauge2", 12.13, config2);
+    loadLiquidFillGauge("fillgauge4", 33.32, config2);
+    loadLiquidFillGauge("fillgauge5", 66, config2);
+
+
+    
 
     function NewValue(){
         if(Math.random() > .5){
